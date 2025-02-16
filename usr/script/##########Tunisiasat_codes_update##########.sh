0c2c94e73bc125fd4666b842efcecab833a58c4c
eliesat <112230228+eliesat@users.noreply.github.com>
2025-02-16 11:34:20 +0200

    Add files via upload

--- usr/script/##########Tunisiasat_codes_update##########.sh
+++ usr/script/##########Tunisiasat_codes_update##########.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wget -q "--no-check-certificate" https://raw.githubusercontent.com/wliesat/scripts/main/installer.sh -O - | /bin/sh
+wget -q "--no-check-certificate" https://raw.githubusercontent.com/eliesat/scripts/main/installer.sh -O - | /bin/sh
