69ee2d11454b1403ef57e36f85020da5c9dfbd96
eliesat <112230228+eliesat@users.noreply.github.com>
2025-09-29 09:38:02 +0300

    Add files via upload

--- usr/script/Hamdy-MagicPanelPlugin.sh
+++ usr/script/Hamdy-MagicPanelPlugin.sh
@@ -1,3 +1,1 @@
-#!/bin/sh
-
-wget -q "--no-check-certificate" https://gitlab.com/h-ahmed/Panel/-/raw/main/MagicPanel/MagicPanel-install.sh -O - | /bin/sh
+wget -q "--no-check-certificate" https://raw.githubusercontent.com/Ham-ahmed/pro/refs/heads/main/MagicPanelPro_install.sh -O - | /bin/sh
