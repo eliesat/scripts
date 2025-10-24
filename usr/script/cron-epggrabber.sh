cae708491f3a02cbac3f88a827c9586708b15354
eliesat <112230228+eliesat@users.noreply.github.com>
2025-10-24 07:09:14 +0300

    Add files via upload

--- usr/script/cron-epggrabber.sh
+++ usr/script/cron-epggrabber.sh
@@ -4,27 +4,76 @@ echo 1 > /proc/sys/vm/drop_caches
 echo 2 > /proc/sys/vm/drop_caches
 echo 3 > /proc/sys/vm/drop_caches
 
-declare -a scripts=(
-    "arabiapriet5.py"
-    "bein.py"
-    "beinConnect.py"
-    "beincin.py"
-    "beinent.py"
-    "beinentC.py"
-    "elcin.py"
-    "elcinEN.py"
-    "uaeariet5.py"
-    "uaeeniet5.py"
-)
-
-for script in "${scripts[@]}"; do
-    python "/usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/$script"
-    if [ $? -ne 0 ]; then
-        echo "Failed to execute $script"
-        exit 1
-    fi
-done
 
 
-
-
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/arabiapriet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/bein.py
+wait 
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/beinConnect.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/beincin.py
+wait  
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/beinent.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/beinentC.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/beinsportiet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/egypt2iet5.py
+wait  
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/elcin.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/elcinEN.py
+wait  
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/elcinmaiet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/nilesatiet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/osnar.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/osnen.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/osnplay.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/qatar1iet5.py
+wait  
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/qatar2iet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/qatar3iet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/qatar4iet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/qatar5iet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/qatar6iet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/qatarariet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/qatareniet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/qatarpriet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/saudiarabia1iet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/saudiarabia2iet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/saudiarabia3iet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/saudiarabia4iet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/sportiet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/uaeariet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/uaeeniet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/uae1iet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/uae2iet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/uae3iet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/uae4iet5.py
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/uae5iet5.py
